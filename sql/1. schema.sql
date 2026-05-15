3fb46130105f46531d9a8d80a9c0a0b2d9590f68
Daksh Tiwari <tiwaridaksh555@gmail.com>
2026-05-15 18:43:32 +0530

    Update 1. schema.sql

--- sql/1. schema.sql
+++ sql/1. schema.sql
@@ -1,17 +1,17 @@
 CREATE DATABASE IF NOT EXISTS retail;
 USE retail;
+
 DROP TABLE IF EXISTS online_retail;
 
 CREATE TABLE online_retail (
-	invoice_no VARCHAR(20),
-    stockcode VARCHAR(20),
-    description VARCHAR(300),
-    quantity INT,
-    invoice_date DATETIME,
-    unitprice DECIMAL(10,2),
-    customer_id INT,
-    country VARCHAR(30)
+  invoice_no   VARCHAR(20),
+  stockcode    VARCHAR(20),
+  description  VARCHAR(255),
+  quantity     VARCHAR(20),
+  invoice_date VARCHAR(50),
+  unitprice    VARCHAR(20),
+  customer_id  VARCHAR(50),
+  country      VARCHAR(100)
 );
 
-CREATE INDEX idx_customer_date ON online_retail (customer_id, invoice_date);
-CREATE INDEX idx_invoice_no    ON online_retail (invoice_no);
+
